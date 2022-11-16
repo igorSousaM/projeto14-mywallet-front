0d5aaa16bbbf622de86e5a86295475d74f2376a1
Igor-S-M <igorsousa3148@gmail.com>
2022-11-16 16:33:26 -0300

    feat: add forms behavior on SignIn Page

--- src/Pages/SingInPage/SignInPage.js
+++ src/Pages/SingInPage/SignInPage.js
@@ -1,17 +1,35 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import { StyledMain } from "./SignInStyle"
-
 export default function SignInPage() {
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    let navigate = useNavigate()
+
+    function login(e){
+
+        e.preventDefault()
+        e.stopPropagation()
+
+        const body = {
+            email,
+            password
+        }
+
+        console.log(body)
+        navigate("/finances")
+    }
+
+
     return (
         <StyledMain>
             <h1>MyWallet</h1>
-            <form>
-                <input placeholder="E-mail" />
-                <input placeholder="Senha" />
-                <Link to={"/finances"}>
-                <button>Entrar</button>
-                </Link>
-                
+            <form onSubmit={login}>
+                <input required type="text" placeholder="E-mail" onChange={e => setEmail(e.target.value)}/>
+                <input required type="password" placeholder="Senha" onChange={e => setPassword(e.target.value)}/>
+                <button type="submit">Entrar</button>                
             </form>
             <Link to={"/SignUp"}>
             <p>Primeira vez? Cadastre-se!</p>
