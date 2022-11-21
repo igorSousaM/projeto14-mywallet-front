a3697ff40e37f06fff9d3df1409965ee19011b41
Igor-S-M <igorsousa3148@gmail.com>
2022-11-21 12:01:31 -0300

    feat: add loadingState on SignUp

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { StyledMain } from "./SignUptyle";
 import axios from "axios";
 
+
 export default function SignUpPage() {
 
     const [name, setName] = useState("")
@@ -10,12 +11,16 @@ export default function SignUpPage() {
     const [password, setPassword] = useState("") 
     const [passwordConfirm, setPasswordConfirm] = useState("")
 
+    const [loadingState, setLoadingState] = useState(false)
+
     const navigate = useNavigate()
 
     function createAccount(e){
 
         e.preventDefault()
         e.stopPropagation()
+
+        setLoadingState(true)
 
         const body = {
             name,
@@ -25,10 +30,12 @@ export default function SignUpPage() {
 
         axios.post("http://localhost:5000/sign-up",body)
         .then(()=>{
+            setLoadingState(false)
             navigate("/")
         }).catch((err)=>{
             console.log(err)
             alert("preencha os campos de acordo")
+            setLoadingState(false)
         })
 
     }
@@ -38,11 +45,11 @@ export default function SignUpPage() {
         <StyledMain>
             <h1>MyWallet</h1>
             <form onSubmit={createAccount}>
-                <input required type="text" value={name} placeholder="Nome" onChange={e=>setName(e.target.value)}/>
-                <input required type="email" value={email} placeholder="E-mail" onChange={e=>setEmail(e.target.value)}/>
-                <input required type="password" value={password} placeholder="Senha" onChange={e=>setPassword(e.target.value)}/>
-                <input required type="password" value={passwordConfirm} placeholder="Confirme a senha" onChange={e=>setPasswordConfirm(e.target.value)}/>
-                <button type="submit">Cadastrar</button>
+                <input  disabled={loadingState} required type="text" value={name} placeholder="Nome" onChange={e=>setName(e.target.value)}/>
+                <input  disabled={loadingState} required type="email" value={email} placeholder="E-mail" onChange={e=>setEmail(e.target.value)}/>
+                <input  disabled={loadingState} required type="password" value={password} placeholder="Senha" onChange={e=>setPassword(e.target.value)}/>
+                <input  disabled={loadingState} required type="password" value={passwordConfirm} placeholder="Confirme a senha" onChange={e=>setPasswordConfirm(e.target.value)}/>
+                <button disabled={loadingState}  type="submit">Cadastrar</button>
             </form>
             <Link to={"/"}>
                 <p>Já tem uma conta? Entre agora!</p>
